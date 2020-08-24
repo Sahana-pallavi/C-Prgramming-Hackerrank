4f7385d20039293ad8257844117fdccc788fed18
Sahana-pallavi <55314643+Sahana-pallavi@users.noreply.github.com>
2020-08-24 10:35:58 +0530

    Add files via upload

---
+++ Hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char a[100];
+   
+    printf("Hello, World!\n"); 
+    printf("Welcome to C programming.");
+    return 0;
+}
